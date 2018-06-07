030f1d4e0f414ec6753ccb85a249dbdb3eb672fa
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-06-07 21:33:49 +0300

    debugging version of context.go; and rebuilded production version of docker-gen with patch applied (version is 0.7.4.2)

---
+++ docker/go/docker-gen/context.go
@@ -0,0 +1,215 @@
+package dockergen
+
+import (
+	"bufio"
+	"os"
+	"regexp"
+	"sync"
+	"fmt"
+	"github.com/fsouza/go-dockerclient"
+)
+
+var (
+	mu         sync.RWMutex
+	dockerInfo Docker
+	dockerEnv  *docker.Env
+)
+
+type Context []*RuntimeContainer
+
+func (c *Context) Env() map[string]string {
+	return splitKeyValueSlice(os.Environ())
+}
+
+func (c *Context) Docker() Docker {
+	mu.RLock()
+	defer mu.RUnlock()
+	return dockerInfo
+}
+
+func SetServerInfo(d *docker.DockerInfo) {
+	mu.Lock()
+	defer mu.Unlock()
+	dockerInfo = Docker{
+		Name:               d.Name,
+		NumContainers:      d.Containers,
+		NumImages:          d.Images,
+		Version:            dockerEnv.Get("Version"),
+		ApiVersion:         dockerEnv.Get("ApiVersion"),
+		GoVersion:          dockerEnv.Get("GoVersion"),
+		OperatingSystem:    dockerEnv.Get("Os"),
+		Architecture:       dockerEnv.Get("Arch"),
+		CurrentContainerID: GetCurrentContainerID(),
+	}
+}
+
+func SetDockerEnv(d *docker.Env) {
+	mu.Lock()
+	defer mu.Unlock()
+	dockerEnv = d
+}
+
+type Address struct {
+	IP           string
+	IP6LinkLocal string
+	IP6Global    string
+	Port         string
+	HostPort     string
+	Proto        string
+	HostIP       string
+}
+
+type Network struct {
+	IP                  string
+	Name                string
+	Gateway             string
+	EndpointID          string
+	IPv6Gateway         string
+	GlobalIPv6Address   string
+	MacAddress          string
+	GlobalIPv6PrefixLen int
+	IPPrefixLen         int
+}
+
+type Volume struct {
+	Path      string
+	HostPath  string
+	ReadWrite bool
+}
+
+type State struct {
+	Running bool
+}
+
+type RuntimeContainer struct {
+	ID           string
+	Addresses    []Address
+	Networks     []Network
+	Gateway      string
+	Name         string
+	Hostname     string
+	Image        DockerImage
+	Env          map[string]string
+	Volumes      map[string]Volume
+	Node         SwarmNode
+	Labels       map[string]string
+	IP           string
+	IP6LinkLocal string
+	IP6Global    string
+	Mounts       []Mount
+	State        State
+}
+
+func (r *RuntimeContainer) Equals(o RuntimeContainer) bool {
+	return r.ID == o.ID && r.Image == o.Image
+}
+
+func (r *RuntimeContainer) PublishedAddresses() []Address {
+	mapped := []Address{}
+	for _, address := range r.Addresses {
+		if address.HostPort != "" {
+			mapped = append(mapped, address)
+		}
+	}
+	return mapped
+}
+
+type DockerImage struct {
+	Registry   string
+	Repository string
+	Tag        string
+}
+
+func (i *DockerImage) String() string {
+	ret := i.Repository
+	if i.Registry != "" {
+		ret = i.Registry + "/" + i.Repository
+	}
+	if i.Tag != "" {
+		ret = ret + ":" + i.Tag
+	}
+	return ret
+}
+
+type SwarmNode struct {
+	ID      string
+	Name    string
+	Address Address
+}
+
+type Mount struct {
+	Name        string
+	Source      string
+	Destination string
+	Driver      string
+	Mode        string
+	RW          bool
+}
+
+type Docker struct {
+	Name               string
+	NumContainers      int
+	NumImages          int
+	Version            string
+	ApiVersion         string
+	GoVersion          string
+	OperatingSystem    string
+	Architecture       string
+	CurrentContainerID string
+}
+
+func GetCurrentContainerID() string {
+	file, err := os.Open("/proc/self/cgroup")
+//debug: change filename from "/proc/self/cgroup" -> "/tmp/proc-self-cgroup.txt"
+	if err != nil {
+		//debug:fmt.Println("error opening file: /proc/self/cgroup")
+		return ""
+	}
+	fmt.Println("file opened: /proc/self/cgroup")
+	reader := bufio.NewReader(file)
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		_, lines, err := bufio.ScanLines([]byte(scanner.Text()), true)
+		if err == nil {
+			strLines := string(lines)
+			if id := matchDockerCurrentContainerID(strLines); id != "" {
+				//debug:fmt.Println("id: ",id)
+				return id //below was missed (after else if) " id :="
+			} else if id := matchECSCurrentContainerID(strLines); id != "" {
+				//debug:fmt.Println("ECS id: ",id)
+				return id
+			}
+		}
+	}
+
+	return ""
+}
+
+func matchDockerCurrentContainerID(lines string) string {
+	regex := "/docker[/-]([[:alnum:]]{64})(\\.scope)?$"
+	re := regexp.MustCompilePOSIX(regex)
+
+	if re.MatchString(lines) {
+		submatches := re.FindStringSubmatch(string(lines))
+		containerID := submatches[1]
+		//debug:fmt.Println("containerID: ",containerID)
+		return containerID
+	}
+	return ""
+}
+
+func matchECSCurrentContainerID(lines string) string {
+	regex := "/ecs\\/[^\\/]+\\/(.+)$"
+	re := regexp.MustCompilePOSIX(regex)
+
+	if re.MatchString(string(lines)) {
+		submatches := re.FindStringSubmatch(string(lines))
+		containerID := submatches[1]
+		//debug:fmt.Println("ECS containerID: ",containerID)
+		return containerID
+	}
+
+	return ""
+}
