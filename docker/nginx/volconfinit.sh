7a0aa88c5df581c2082bbce96ff569c55f5e0613
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-06-11 15:45:08 +0300

    added template for nginx.conf in nginx dockerfile - now using env variable PHP_UPSTREAM_HOST for fcgi-php backend container name with default value=app

--- docker/nginx/volconfinit.sh
+++ docker/nginx/volconfinit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 if [ ! -f '/usr/local/etc/nginx/nginx.conf' ]; then
-  cp -f /nginx.conf /usr/local/etc/nginx/nginx.conf
+#  cp -f /nginx.conf /usr/local/etc/nginx/nginx.conf
+  /usr/local/bin/confd -onetime -backend env
 fi
 exec "$@"
