556daa67383cab9c815d245e93527c30a61fd70d
Konstantin Babin <37809416+babinkos@users.noreply.github.com>
2018-06-27 18:59:43 +0300

    dbclone editing script

--- docker/php-fpm/autobuild/dbrelease.sh
+++ docker/php-fpm/autobuild/dbrelease.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 set -eu
+echo "$NEXTCLOUD_VERSION"
+echo "$POSTGRES_DB"
+#13.0.2
+PGHOST=$POSTGRES_HOST
+pg_dump -O $POSTGRES_DB /tmp/$POSTGRES_DB.sql
+PGDATABASE="ncdb_$(echo "$NEXTCLOUD_VERSION" | tr . _)"
