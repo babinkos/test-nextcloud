7bc1b8cf3e3708e5432822776bd97cd5a79f93a0
q0s <37809416+babinkos@users.noreply.github.com>
2018-07-07 00:35:16 +0300

    for test lets install nc with occ

--- docker/php-fpm/autobuild/test-nc-status.sh
+++ docker/php-fpm/autobuild/test-nc-status.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -eu
-if [ "$(pidof php-fpm)" == "" ]; then
+if [ "$(pidof php-fpm)" = "" ]; then
     echo "php-fpm start"
     php-fpm&
     sleep 2
   else
     echo "php-fpm already started";
 fi
-env
-SCRIPT_FILENAME=/var/www/html/status.php REQUEST_URI=/ QUERY_STRING= REQUEST_METHOD=GET /usr/bin/cgi-fcgi -bind -connect 127.0.0.1:9000
-sleep 5
-SCRIPT_FILENAME=/var/www/html/index.php REQUEST_URI=/ QUERY_STRING= REQUEST_METHOD=GET /usr/bin/cgi-fcgi -bind -connect 127.0.0.1:9000
-sleep 5
-SCRIPT_FILENAME=/var/www/html/status.php REQUEST_URI=/ QUERY_STRING= REQUEST_METHOD=GET /usr/bin/cgi-fcgi -bind -connect 127.0.0.1:9000
+#env
+#SCRIPT_FILENAME=/var/www/html/status.php REQUEST_URI=/ QUERY_STRING= REQUEST_METHOD=GET /usr/bin/cgi-fcgi -bind -connect 127.0.0.1:9000
+#sleep 5
+#SCRIPT_FILENAME=/var/www/html/index.php REQUEST_URI=/ QUERY_STRING= REQUEST_METHOD=GET /usr/bin/cgi-fcgi -bind -connect 127.0.0.1:9000
+sudo -u www-data php /var/www/html/occ maintenance:install -n
+#sleep 5
+SCRIPT_FILENAME=/var/www/html/status.php REQUEST_URI=/ QUERY_STRING='' REQUEST_METHOD=GET /usr/bin/cgi-fcgi -bind -connect 127.0.0.1:9000
